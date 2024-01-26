3841003427a92895737938772f8befdb26c0dffa
admin <hirak.keisuke@gmail.com>
2024-01-26 16:21:10 +0900

    [Add and initialize Swiper]

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+/////////////////// HAMBURGER /////////////////////
 const button = document.getElementsByClassName("hamburger-button");
 const nav = document.getElementById("nav");
 const toggleSections = document.querySelectorAll(".action-link");
@@ -21,4 +22,27 @@ mask.addEventListener("click", () => {
   button[0].classList.remove("open");
   nav.classList.remove("open");
   mask.classList.remove("open");
+});
+
+/////////////////// SWIPER /////////////////////
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
