53fc4a5d78956145d164b658bb22290bd37972b5
admin <hirak.keisuke@gmail.com>
2024-01-29 09:27:56 +0900

    [Adjust hamburger menu]

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,17 @@
 const button = document.getElementsByClassName("hamburger-button");
 const nav = document.getElementById("nav");
 const toggleSections = document.querySelectorAll(".action-link");
-const mask = document.getElementById("mask");
 
 button[0].addEventListener("click", () => {
   button[0].classList.toggle("open");
   nav.classList.toggle("open");
-  mask.classList.toggle("open");
 });
 
 toggleSections.forEach((toggleSections) => {
   toggleSections.addEventListener("click", () => {
     button[0].classList.remove("open");
     nav.classList.remove("open");
-    mask.classList.remove("open");
   });
-});
-
-mask.addEventListener("click", () => {
-  button[0].classList.remove("open");
-  nav.classList.remove("open");
-  mask.classList.remove("open");
 });
 
 /////////////////// SPLIDE /////////////////////
